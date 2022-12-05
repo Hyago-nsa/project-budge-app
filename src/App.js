eb6f64ffc87a2586be51752703fc56ab163264e6
Hyago-nsa <hyago.eurico.nsa@gmail.com>
2022-12-05 19:49:21 -0300

    Add Expenses functionality

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React, { Fragment, useState } from "react";
 import { Container, Stack, Button } from "react-bootstrap";
 import "./App.css";
 import AddBudgetModal from "./components/AddBudgetModal";
+import AddExpenseModal from "./components/AddExpenseModal";
 import BudgetCard from "./components/BudgetCard";
 import { useBudgets } from "./contexts/BudgetsContext";
 
 const App = () => {
   const [showAddBudgetModal, setShowAddBudgetModal] = useState(false);
+  const [showAddExpenseModal, setShowAddExpenseModal] = useState(false);
+  const [addExpenseModalBudgetId, setAddExpenseModalBudgetId] = useState();
   const { budgets, getBudgetExpenses } = useBudgets();
+
+  function openAddExpenseModal(budgetId){
+    setShowAddExpenseModal(true)
+    setAddExpenseModalBudgetId(budgetId)
+  }
 
   return (
     <Fragment>
@@ -17,7 +25,7 @@ const App = () => {
           <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>
             Add Budget
           </Button>
-          <Button variant="outline-primary">Add Expense</Button>
+          <Button variant="outline-primary" onClick={openAddExpenseModal} >Add Expense</Button>
         </Stack>
         <div
           style={{
@@ -38,6 +46,7 @@ const App = () => {
                 name={budget.name}
                 amount={amount}
                 max={budget.max}
+                onAddExpenseClick={()=> openAddExpenseModal(budget.id)}
               />
             );
           })}
@@ -46,6 +55,11 @@ const App = () => {
       <AddBudgetModal
         show={showAddBudgetModal}
         handleClose={() => setShowAddBudgetModal(false)}
+      />
+      <AddExpenseModal
+        show={showAddExpenseModal}
+        defaultBudgetId={addExpenseModalBudgetId}
+        handleClose={() => setShowAddExpenseModal(false)}
       />
     </Fragment>
   );
